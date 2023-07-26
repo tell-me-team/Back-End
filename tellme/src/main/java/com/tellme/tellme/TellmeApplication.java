81055b9df65f8f5286d3d461ac94b58a4b432117
Jsim6342 <snowdrop6342@gmail.com>
2023-07-26 20:53:57 +0900

    docker 파일 추가

--- tellme/src/main/java/com/tellme/tellme/TellmeApplication.java
+++ tellme/src/main/java/com/tellme/tellme/TellmeApplication.java
@@ -2,8 +2,9 @@ package com.tellme.tellme;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class TellmeApplication {
 
 	public static void main(String[] args) {
