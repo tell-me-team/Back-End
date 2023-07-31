5c2e05544114aac6922f075993dd50da05a84c78
youngyoung0 <taeyoung6873@gmail.com>
2023-07-31 21:11:59 +0900

    feat: [#7] 설문결과 API 개발

--- tellme/src/main/java/com/tellme/tellme/domain/survey/presentation/SurveyController.java
+++ tellme/src/main/java/com/tellme/tellme/domain/survey/presentation/SurveyController.java
@@ -10,12 +10,14 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class SurveyController {
 
-    private SurveyService surveyService;
+    private final SurveyService surveyService;
 
     @PostMapping("/{surveyId}/{userId}")
-    private void saveAnswer(@PathVariable("surveyId") int surveyId,
+    @ResponseBody
+    private ResponseEntity saveAnswer(@PathVariable("surveyId") int surveyId,
                                       @PathVariable("userId") String userId,
                                       @RequestBody SurveyDto.Answer answer){
         surveyService.saveAnswer(surveyId, userId, answer);
+        return ResponseEntity.ok().build();
     }
 }
