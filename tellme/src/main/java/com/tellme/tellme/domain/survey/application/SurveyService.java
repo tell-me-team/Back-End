6a96c78f162a5eb8112db7baee38887eda2d46a4
youngyoung0 <taeyoung6873@gmail.com>
2023-07-31 21:11:59 +0900

    feat: [#7] 설문결과 API 개발

--- tellme/src/main/java/com/tellme/tellme/domain/survey/application/SurveyService.java
+++ tellme/src/main/java/com/tellme/tellme/domain/survey/application/SurveyService.java
@@ -1,21 +1,25 @@
 package com.tellme.tellme.domain.survey.application;
 
-import com.tellme.tellme.domain.survey.entity.Survey;
+import com.tellme.tellme.domain.survey.entity.Question;
+import com.tellme.tellme.domain.survey.entity.SurveyAnswer;
 import com.tellme.tellme.domain.survey.entity.SurveyCompletion;
-import com.tellme.tellme.domain.survey.persistence.SurveyAnswerRepository;
-import com.tellme.tellme.domain.survey.persistence.SurveyCompletionRepository;
-import com.tellme.tellme.domain.survey.persistence.SurveyRepository;
+import com.tellme.tellme.domain.survey.persistence.*;
 import com.tellme.tellme.domain.survey.presentation.SurveyDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class SurveyService {
 
     private final SurveyCompletionRepository surveyCompletionRepository;
+    private final SurveyCompletionQueryRepository surveyCompletionQueryRepository;
     private final SurveyAnswerRepository surveyAnswerRepository;
     private final SurveyRepository surveyRepository;
+    private final QuestionRepository questionRepository;
+
 
 
     public void saveAnswer(int surveyId, String userId, SurveyDto.Answer answer) {
@@ -23,7 +27,14 @@ public class SurveyService {
 
         SurveyCompletion surveyCompletion = surveyCompletionRepository.save(answer.toSurveyCompletion());
         for (SurveyDto.AnswerContent answerContent : answer.getAnswerContentList()) {
-            surveyAnswerRepository.save(answerContent.toSurveyAnswer(surveyCompletion));
+            Question question = questionRepository.findById(answerContent.getQuestion()).get();
+            surveyAnswerRepository.save(answerContent.toSurveyAnswer(surveyCompletion,question));
         }
+    }
+
+    public void getSurveyResult(int userId, int surveyId) {
+        SurveyCompletion surveyCompletion = surveyCompletionQueryRepository.findByUserIdAndSurveyId(userId, surveyId);
+        List<SurveyAnswer> surveyAnswerList = surveyAnswerRepository.findBySurveyCompletion(surveyCompletion);
+
     }
 }
