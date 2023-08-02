db13ddfcda38bcf888f5930d8a23b6771fb0d321
youngyoung0 <taeyoung6873@gmail.com>
2023-08-03 08:58:14 +0900

    feat: swagger @Operation 추가

--- tellme/src/main/java/com/tellme/tellme/domain/survey/presentation/SurveyController.java
+++ tellme/src/main/java/com/tellme/tellme/domain/survey/presentation/SurveyController.java
@@ -3,6 +3,7 @@ package com.tellme.tellme.domain.survey.presentation;
 import com.tellme.tellme.common.response.BaseResponse;
 import com.tellme.tellme.domain.survey.application.SurveyService;
 import com.tellme.tellme.domain.survey.entity.SurveyCompletion;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
@@ -17,6 +18,7 @@ public class SurveyController {
     private final SurveyService surveyService;
 
     @PostMapping("/{surveyId}/{userId}/answer")
+    @Operation(summary = "설문 답변")
     @ResponseBody
     public BaseResponse<SurveyCompletion> saveAnswer(@PathVariable("surveyId") int surveyId,
                                                      @PathVariable("userId") int userId,
@@ -26,6 +28,7 @@ public class SurveyController {
     }
 
     @GetMapping("/share/{surveyId}")
+    @Operation(summary = "설문 링크 공유 url 생성")
     @ResponseBody
     public BaseResponse<String> share(@PathVariable("surveyId") int surveyId,
                                        Authentication authentication){
@@ -34,12 +37,14 @@ public class SurveyController {
     }
 
     @GetMapping("/{short_url}")
+    @Operation(summary = "설문 링크 url 디코드")
     @ResponseBody
     public BaseResponse<SurveyDto.SurveyInfo> shortUrlDecoding(@PathVariable("short_url") String shortUrl){
         return BaseResponse.ok(surveyService.shortUrlDecoding(shortUrl));
     }
 
     @GetMapping("/question/{survey_id}")
+    @Operation(summary = "설문 질문 리스트")
     @ResponseBody
     public BaseResponse<List<SurveyDto.QuestionInfo>> getQuestionInfo(@PathVariable("survey_id") int surveyId){
         return BaseResponse.ok(surveyService.getQuestionInfo(surveyId));
--- tellme/src/main/java/com/tellme/tellme/healcheck/healthcheckController.java
+++ tellme/src/main/java/com/tellme/tellme/healcheck/healthcheckController.java
@@ -1,11 +1,14 @@
 package com.tellme.tellme.healcheck;
 
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class healthcheckController {
+
     @GetMapping("/healthcheck")
+    @Operation(summary = "서버 상태 확인 체크")
     public String healthcheck(){
         return "ok";
     }
