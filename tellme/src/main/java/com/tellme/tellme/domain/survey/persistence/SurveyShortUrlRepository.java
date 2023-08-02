904ba22e767f6b121a85a428646f0727af921ae9
youngyoung0 <taeyoung6873@gmail.com>
2023-08-02 23:55:10 +0900

    feat: [#7] 설문 공유하기 단축Url 암호화

---
+++ tellme/src/main/java/com/tellme/tellme/domain/survey/persistence/SurveyShortUrlRepository.java
@@ -0,0 +1,10 @@
+package com.tellme.tellme.domain.survey.persistence;
+
+import com.tellme.tellme.domain.survey.entity.SurveyShortUrl;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SurveyShortUrlRepository extends JpaRepository<SurveyShortUrl, Integer> {
+    SurveyShortUrl findBySurveyIdAndUserId(int surveyId, long userId);
+}
