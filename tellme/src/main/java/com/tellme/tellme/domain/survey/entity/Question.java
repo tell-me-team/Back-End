e98fc58bea5d6c47f23bde463b46c59292086149
youngyoung0 <taeyoung6873@gmail.com>
2023-08-02 21:40:41 +0900

    feat: 테이블 생성시 테이블명이 대문자로 들어가고, 컬럼이 카멜케이스로 들어가지 않아서 수정했습니다.

--- tellme/src/main/java/com/tellme/tellme/domain/survey/entity/Question.java
+++ tellme/src/main/java/com/tellme/tellme/domain/survey/entity/Question.java
@@ -6,15 +6,17 @@ import lombok.Getter;
 
 @Entity
 @Getter
+@Table(name = "question")
 public class Question extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    @Column(name = "order_number")
     private int orderNumber;
     @Column(length = 500)
     private String question;
-    @Column(length = 300)
+    @Column(length = 300, name = "answer_a")
     private String answerA;
-    @Column(length = 300)
+    @Column(length = 300, name = "answer_b")
     private String answerB;
 }
--- tellme/src/main/java/com/tellme/tellme/domain/survey/entity/SurveyQuestion.java
+++ tellme/src/main/java/com/tellme/tellme/domain/survey/entity/SurveyQuestion.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 
 @Entity
 @Getter
+@Table(name = "survey_question")
 public class SurveyQuestion extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
