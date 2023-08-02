a8a5e1784d63aa62526079c831def1e6cbf1306e
youngyoung0 <taeyoung6873@gmail.com>
2023-08-02 11:39:21 +0900

    feat: [#7] API 버전 관리하기 위해서 수정

--- tellme/src/main/java/com/tellme/tellme/domain/survey/presentation/SurveyController.java
+++ tellme/src/main/java/com/tellme/tellme/domain/survey/presentation/SurveyController.java
@@ -6,7 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/survey")
+@RequestMapping("v1/survey")
 @RequiredArgsConstructor
 public class SurveyController {
 
