8e778a059b7c9b341729570c1c65d413a925e229
youngyoung0 <taeyoung6873@gmail.com>
2023-08-03 16:07:41 +0900

    fix: 설문조사 중복시 예외 달리도록 수정

--- tellme/src/main/java/com/tellme/tellme/common/exception/ErrorStatus.java
+++ tellme/src/main/java/com/tellme/tellme/common/exception/ErrorStatus.java
@@ -11,7 +11,10 @@ public enum ErrorStatus {
     // COMMON
     RESOURCE_NOT_VALID(HttpStatus.NOT_FOUND, "요청한 리소스를 찾을 수 없습니다."),
     CLIENT_REQUEST_ERROR(HttpStatus.BAD_REQUEST, "잘못된 요청입니다. 요청을 확인해주세요."),
-    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버에 문제가 생겼습니다. 잠시 후 다시 시도해주세요.");
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버에  문제가 생겼습니다. 잠시 후 다시 시도해주세요."),
+
+    // SURVEY
+    SURVEY_ALREADY_COMPLETED(HttpStatus.CONFLICT, "이미 설문조사에 참여했습니다.");
 
     private final HttpStatus status;
     private final String message;
