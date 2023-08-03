3e7a8d8dfc21ea701c72436127c83e8c0ede5964
Jsim6342 <snowdrop6342@gmail.com>
2023-08-03 22:43:26 +0900

    Feat:[#14] 인증 필터 예외처리 구현

--- tellme/src/main/java/com/tellme/tellme/common/auth/JwtAuthenticationFilter.java
+++ tellme/src/main/java/com/tellme/tellme/common/auth/JwtAuthenticationFilter.java
@@ -1,10 +1,13 @@
-package com.tellme.tellme.common.config;
+package com.tellme.tellme.common.auth;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.tellme.tellme.common.exception.BaseException;
+import com.tellme.tellme.common.response.BaseExceptionResponse;
+import com.tellme.tellme.common.response.BaseResponse;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +16,10 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.stream.Collectors;
+
+import static com.tellme.tellme.common.exception.ErrorStatus.EXPIRED_ACCESS_TOKEN;
+
 
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
@@ -30,16 +37,18 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest servletRequest, HttpServletResponse servletResponse, FilterChain filterChain) throws ServletException, IOException {
-        String token = jwtTokenProvider.resolveToken(servletRequest);
-        LOGGER.info("[doFilterInternal] token 값 추출 완료. token : {}", token);
+        try {
+            String token = jwtTokenProvider.resolveToken(servletRequest);
+            LOGGER.info("[doFilterInternal] token 값 추출 완료. token : {}", token);
 
-        // TODO. Refresh 재발급 로직 추가
-
-        LOGGER.info("[doFilterInternal] token 값 유효성 체크 시작");
-        if (token != null && jwtTokenProvider.validateToken(token)) {
-            Authentication authentication = jwtTokenProvider.getAuthentication(token);
-            SecurityContextHolder.getContext().setAuthentication(authentication);
-            LOGGER.info("[doFilterInternal] token 값 유효성 체크 완료");
+            LOGGER.info("[doFilterInternal] token 값 유효성 체크 시작");
+            if (token != null && jwtTokenProvider.validateToken(token)) {
+                Authentication authentication = jwtTokenProvider.getAuthentication(token);
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+                LOGGER.info("[doFilterInternal] token 값 유효성 체크 완료");
+            }
+        }catch (Exception exception) {
+            servletRequest.setAttribute("exception", exception);
         }
 
         filterChain.doFilter(servletRequest, servletResponse);
