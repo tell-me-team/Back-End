3c5ce1992b01859a981cf46dd847b4ed501c718d
Jsim6342 <snowdrop6342@gmail.com>
2023-08-06 17:16:51 +0900

    deploy test

--- tellme/src/main/java/com/tellme/tellme/TellmeApplication.java
+++ tellme/src/main/java/com/tellme/tellme/TellmeApplication.java
@@ -3,12 +3,17 @@ package com.tellme.tellme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class TellmeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TellmeApplication.class, args);
+
+		new SpringApplicationBuilder(TellmeApplication.class)
+				.properties("spring.config.location = classpath:/application.yml, classpath:/env.yml")
+						.run(args);
+//		SpringApplication.run(TellmeApplication.class, args);
 	}
 
 }
