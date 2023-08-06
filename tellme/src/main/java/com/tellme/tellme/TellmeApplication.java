8be428fb7037003375f02c32f8b7b613ba9b4477
Jsim6342 <snowdrop6342@gmail.com>
2023-08-06 22:11:30 +0900

    Bug:[#34] CI/CD 설정 수정
    Update and rename main.yml to deploy.yml
    
    Update deploy.yml

--- tellme/src/main/java/com/tellme/tellme/TellmeApplication.java
+++ tellme/src/main/java/com/tellme/tellme/TellmeApplication.java
@@ -3,11 +3,16 @@ package com.tellme.tellme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class TellmeApplication {
 
 	public static void main(String[] args) {
+
+//		new SpringApplicationBuilder(TellmeApplication.class)
+//				.properties("spring.config.location = classpath:/application.yml, classpath:/env.properties")
+//						.run(args);
 		SpringApplication.run(TellmeApplication.class, args);
 	}
 
